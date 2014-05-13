6374cbf0e499accb91973d4da6ce560d14f50740
Yin Wang <yinwang0@gmail.com>
2014-05-13 11:46:01 -0700

    add ParserException

---
+++ src/main/java/org/yinwang/yin/parser/ParserException.java
@@ -0,0 +1,22 @@
+package org.yinwang.yin.parser;
+
+
+public class ParserException extends Exception {
+    public int line;
+    public int col;
+    public int start;
+
+
+    public ParserException(String message, int line, int col, int start) {
+        super(message);
+        this.line = line;
+        this.col = col;
+        this.start = start;
+    }
+
+
+    @Override
+    public String toString() {
+        return line + ":" + col + " parsing error " + getMessage();
+    }
+}
